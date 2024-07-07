12d32f38e5f3a94dc78ecaa1f3a251b7a3caa202
zinmi <zinmi633@gmail.com>
2024-07-07 19:04:26 +1000

    доработана система редактирования, добавлена кнопка delete

--- scripts.js
+++ scripts.js
@@ -54,24 +54,24 @@ function edit(i){
        <span>Count: <input type="text" name="Имя" id="edited count"></span> 
         <button id="cancel button">cancel</button>
        <button id="save button">save</button>
+       <button id="delete button">delete</button>
         `)
     const editedName = document.getElementById('edited name')
     const editedCount =document.getElementById('edited count')
     const canselButton = document.getElementById('cancel button')
     const saveButton = document.getElementById('save button')
+    const deleteButton = document.getElementById('delete button')
+    deleteButton.onclick = () => {
+        leaderboard.splice(i,1)
+        render()
+        placeOfEditing.textContent = ''
+    }
     canselButton.onclick = ()=>{
         placeOfEditing.textContent = ''
     }
     saveButton.onclick = ()=>{
-        if(editedName.value == '' || editedCount == ''){
-            alert('все поля должны быть заполнены')
-            return
-        }else if(isNaN(Number(editedCount.value))){
-            alert('некоректно заполнены данные')
-            return
-        }
-        leaderboard[i].name = editedName.value
-        leaderboard[i].count = Number(editedCount.value)
+        editedName.value == '' ? console.log('параметр name не изменён') :leaderboard[i].name = editedName.value
+        isNaN(Number(editedCount.value)) || editedCount.value == '' ? console.log('параметр count не изменён') :leaderboard[i].count = Number(editedCount.value)
         render()
         placeOfEditing.textContent = ''
     }
